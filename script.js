1bd731eafb77f2c5a885b2a376b7538ad557352d
LeoLacana <wilson17414@gmail.com>
2021-04-10 18:39:59 -0300

    requisito 2

---
+++ script.js
@@ -0,0 +1,5 @@
+let elementColor = document.getElementsByClassName('color');
+elementColor[0].style.backgroundColor = 'black';
+elementColor[1].style.backgroundColor = 'red';
+elementColor[2].style.backgroundColor = 'blue';
+elementColor[3].style.backgroundColor = 'green';
