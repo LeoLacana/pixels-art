85fa08d2bdcea4f63437dfaea48728672df4c5ce
LeoLacana <wilson17414@gmail.com>
2021-04-12 17:00:03 -0300

    req 6

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-/*let elementPixel = document.getElementsByClassName('pixel');
-let elementPixelBox = undefined;
-for (let index = 0; index < elementPixel.length; index += 1){
-  for (let index = 0; index < elementPixel.length; index += 1){
-    elementPixelBox = document.createElement('div');
-    elementPixelBox.className = 'box';
-    elementPixel[index].appendChild(elementPixelBox);
-  }
-}*/
-
+window.onload = function() {
+  let brushTool = document.createElement('div');
+  brushTool.className = 'selected';
+  brushTool.style.backgroundColor = 'black';
+}
 const elementPixel = document.getElementById('pixel-board');
 let elementPixelBox;
 for (let index = 0; index < 25; index += 1) {
