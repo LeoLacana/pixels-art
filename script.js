48e5c0a539ebc0f4abf73742c51214d44ae56d15
LeoLacana <wilson17414@gmail.com>
2021-04-13 19:52:47 -0300

    req 8

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const elementPixel = document.getElementById('pixel-board');
-let elementPixelBox;
-for (let index = 0; index < 25; index += 1) {
-  elementPixelBox = document.createElement('div');
-  elementPixelBox.className = 'pixel';
-  elementPixel.appendChild(elementPixelBox);
-}
-
 const elementBrushTool = document.getElementById('black');
 elementBrushTool.className += ' selected';
 
@@ -13,8 +5,9 @@ const elementSelecRed = document.getElementById('red');
 const elementSelecBlue = document.getElementById('blue');
 const elementSelecGreen = document.getElementById('green');
 
+const elementClassColor = document.getElementsByClassName('color');
+
 function selectedColor(targetClicado) {
-  const elementClassColor = document.getElementsByClassName('color');
   for (let index = 0; index < elementClassColor.length; index += 1) {
     if (elementClassColor[index].classList.contains('selected') === true) {
       elementClassColor[index].classList.remove('selected');
@@ -29,3 +22,25 @@ elementBrushTool.addEventListener('click', selectedColor);
 elementSelecRed.addEventListener('click', selectedColor);
 elementSelecBlue.addEventListener('click', selectedColor);
 elementSelecGreen.addEventListener('click', selectedColor);
+
+const elementPixel = document.getElementById('pixel-board');
+let elementPixelBox;
+for (let index = 0; index < 25; index += 1) {
+  elementPixelBox = document.createElement('div');
+  elementPixelBox.className = 'pixel';
+  elementPixel.appendChild(elementPixelBox);
+}
+
+function colorise(coloriseClick) {
+  for (let index = 0; index < elementClassColor.length; index += 1){
+    if (elementClassColor[index].classList.contains('selected') === true){
+      coloriseClick.target.id = elementClassColor[index].id;
+    }
+  }
+}
+
+let elementClick = document.getElementsByClassName('pixel');
+
+for (let index = 0; index < elementClick.length; index += 1) {
+  elementClick[index].addEventListener('click', colorise);
+}
