4fa9d080005a983a4187fd8abc6006d96d97c051
LeoLacana <wilson17414@gmail.com>
2021-04-13 17:59:26 -0300

    req 7

--- script.js
+++ script.js
@@ -6,5 +6,29 @@ for (let index = 0; index < 25; index += 1) {
   elementPixel.appendChild(elementPixelBox);
 }
 
-let elementBrushTool = document.getElementById('black');
-elementBrushTool.className += " selected";
+const elementBrushTool = document.getElementById('black');
+elementBrushTool.className += ' selected';
+
+const elementSelecRed = document.getElementById('red');
+const elementSelecBlue = document.getElementById('blue');
+const elementSelecGreen = document.getElementById('green');
+
+elementBrushTool.addEventListener('click', selectedColor);
+elementSelecRed.addEventListener('click', selectedColor);
+elementSelecBlue.addEventListener('click', selectedColor);
+elementSelecGreen.addEventListener('click', selectedColor);
+
+
+function selectedColor (targetClicado) {
+  let elementClassColor = document.getElementsByClassName('color');
+  for (index = 0; index < elementClassColor.length; index += 1){
+    if (elementClassColor[index].classList.contains('selected') === true){
+      elementClassColor[index].classList.remove('selected');
+    }
+  }
+  if (targetClicado.target.classList.contains('selected') !== true){
+    targetClicado.target.classList.add('selected');
+  }
+  
+  
+}
